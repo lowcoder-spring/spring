8ea3c56ef2f073a7d5c2a10e3bc80852bc39bf83
chenyang <chenyang@chinanuoyi.cn>
2022-10-11 16:04:31 +0800

    fix: 解析jwt-scope权限出错

--- commons/resource-server-spring-cloud/resource-server-spring-cloud-autoconfigure/src/main/java/icu/lowcoder/spring/cloud/resource/server/jwt/GrantedAuthoritiesExtractor.java
+++ commons/resource-server-spring-cloud/resource-server-spring-cloud-autoconfigure/src/main/java/icu/lowcoder/spring/cloud/resource/server/jwt/GrantedAuthoritiesExtractor.java
@@ -5,17 +5,17 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.stream.Collectors;
 
 public class GrantedAuthoritiesExtractor implements Converter<Jwt, Collection<GrantedAuthority>> {
 
     @Override
     public Collection<GrantedAuthority> convert(Jwt jwt) {
-        Collection<?> authorities = (Collection<?>)
-                jwt.getClaims().getOrDefault("scope", Collections.emptyList());
+        String scope = (String) jwt.getClaims().getOrDefault("scope", "");
 
+        Collection<String> authorities = Arrays.asList(scope.split(" "));
         return authorities.stream()
                 .map(Object::toString)
                 .map(SimpleGrantedAuthority::new)
