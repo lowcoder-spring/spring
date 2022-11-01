d41f58ab6513e3c1ddac1139098c882c18a1e311
chenyang <chenyang@chinanuoyi.cn>
2022-11-01 14:06:13 +0800

    fix(commons-cloud-api): 配置key错误

--- commons/cloud-api-spring-boot/cloud-api-spring-boot-autoconfigure/src/main/java/icu/lowcoder/spring/commos/cloudapi/ali/sly/AliSlyCloudApiAutoConfiguration.java
+++ commons/cloud-api-spring-boot/cloud-api-spring-boot-autoconfigure/src/main/java/icu/lowcoder/spring/commos/cloudapi/ali/sly/AliSlyCloudApiAutoConfiguration.java
@@ -1,10 +1,12 @@
 package icu.lowcoder.spring.commos.cloudapi.ali.sly;
 
 
-import icu.lowcoder.spring.commos.cloudapi.*;
+import icu.lowcoder.spring.commos.cloudapi.ApiName;
+import icu.lowcoder.spring.commos.cloudapi.BankCardApi;
+import icu.lowcoder.spring.commos.cloudapi.CloudApiProperties;
+import icu.lowcoder.spring.commos.cloudapi.RealNameApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -16,17 +18,39 @@ import org.springframework.web.client.RestTemplate;
 public class AliSlyCloudApiAutoConfiguration {
 
     @Bean("aliSlyRealNameApi")
-    @ConditionalOnProperty(prefix = "icu.lowcoder.spring.commons.cloudapi.apis.REAL_NAME.providers", name = "ali-sly")
+    @ConditionalOnProperty(
+            prefix = "icu.lowcoder.spring.commons.cloudapi.apis.REAL_NAME.providers.aliSly",
+            name = "enabled",
+            havingValue = "true",
+            matchIfMissing = true
+    )
     RealNameApi aliSlyRealNameApiImpl(CloudApiProperties cloudApiProperties,
-                                      @Autowired @Qualifier("cloudApiRestTemplate")RestTemplate restTemplate) {
-        return new AliSlyRealNameApiImpl(restTemplate, cloudApiProperties.getApis().get(ApiName.REAL_NAME).getProviders().get(AliSlyProvider.NAME).getMeta());
+                                      @Autowired @Qualifier("cloudApiRestTemplate") RestTemplate restTemplate) {
+        return new AliSlyRealNameApiImpl(restTemplate,
+                cloudApiProperties.getApis()
+                        .get(ApiName.REAL_NAME)
+                        .getProviders()
+                        .get(AliSlyProvider.NAME)
+                        .getMeta()
+        );
     }
 
     @Bean("aliSlyBankCardApi")
-    @ConditionalOnProperty(prefix = "icu.lowcoder.spring.commons.cloudapi.apis.BANK_CARD.providers", name = "ali-sly")
+    @ConditionalOnProperty(
+            prefix = "icu.lowcoder.spring.commons.cloudapi.apis.BANK_CARD.providers.aliSly",
+            name = "enabled",
+            havingValue = "true",
+            matchIfMissing = true
+    )
     BankCardApi aliSlyBankCardApiImpl(CloudApiProperties cloudApiProperties,
-                                      @Autowired @Qualifier("cloudApiRestTemplate")RestTemplate restTemplate) {
-        return new AliSlyBankCardApiImpl(restTemplate, cloudApiProperties.getApis().get(ApiName.BANK_CARD).getProviders().get(AliSlyProvider.NAME).getMeta());
+                                      @Autowired @Qualifier("cloudApiRestTemplate") RestTemplate restTemplate) {
+        return new AliSlyBankCardApiImpl(restTemplate,
+                cloudApiProperties.getApis()
+                        .get(ApiName.BANK_CARD)
+                        .getProviders()
+                        .get(AliSlyProvider.NAME)
+                        .getMeta()
+        );
     }
 
 }
